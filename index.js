ac4ac423d1561fad4b5dbac5e665a844a7a6b8e8
Maya Borrero <mlbsindia@gmail.com>
2021-09-21 16:00:41 -0400

    commit tree 45048ea12bb4424f54475e689f146bab913f2234

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2; 
+let multiply = (num1 * num2);
+console.log(multiply);
 
-// Write your code here
+let random = (Math.round((Math.random()*10)));
+Number.isInteger(random);
+console.log(random);
+
+const num3 = 16;
+const num4 = 4;
+let mod = num3 / num4;
+
+let max = Math.max(2, 7, 11, 20);
